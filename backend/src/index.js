1745ca650f124f2f183c829dda2999b88796c042
Ankux <ankuxkr@gmail.com>
2025-06-24 23:48:50 +0530

    Routes: Login, Logout, Update Profile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -7,8 +8,10 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 
+
 // middleware
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
