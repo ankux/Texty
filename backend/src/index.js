b6d6f314f63d4c66befae85d52ea08f70b979aea
Ankux <ankuxkr@gmail.com>
2025-06-25 20:27:38 +0530

    Sign up and Login Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -13,6 +14,10 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,64 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+
+            set({ authUser: res.data.user });
+        } catch (error) {
+            console.log("Error in checkAuth: ", error.message);
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (formData) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", formData);
+            console.log("Signup response:", res.data);
+            set({ authUser: res.data });
+            toast.success("Signed up successfully");
+        } catch (error) {
+            console.log("Error in signup useAuthStore: ", error.message);
+            toast.error("Something went wrong");
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    login: async (formData) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", formData);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+        } catch (error) {
+            console.log("Error in login useAuthStore: ", error.message);
+            toast.error("Something went wrong");
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            console.log("Error in logout useAuthStore: ", error.message);
+            toast.error("Something went wrong");
+        }
+    },
+}))
