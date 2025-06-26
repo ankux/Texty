85dfbb3c41a1d49d98ddcf865157df1134718215
Ankux <ankuxkr@gmail.com>
2025-06-26 07:22:10 +0530

    Deployment Settings

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,16 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-app
+
+const __dirname = path.resolve();
 
 
 // middleware
@@ -22,6 +25,11 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")));
+}
 
 const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
