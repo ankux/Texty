87dad73bdb8230a4a66d5cce40b17da584faa898
Ankux <ankuxkr@gmail.com>
2025-06-26 07:37:54 +0530

    Bug fixes

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -26,5 +26,6 @@ export const protectRoute = async (req, res, next) => {
 
     } catch (error) {
         console.log("Error in protectRoute middleware: ", error.message);
+        return res.status(401).json({ message: "Unauthorized - Invalid token" });
     }
 }
